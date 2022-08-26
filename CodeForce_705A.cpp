789d867e7396a1e8762c60f82f98e18080268404
Ashiq <md.e.h.ashiq@gmail.com>
2022-08-26 19:07:01 +0600

    705A

---
+++ CodeForce_705A.cpp
@@ -0,0 +1,20 @@
+// 705A
+
+#include <iostream>
+using namespace std;
+int a;
+int main() {
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        if (i % 2 == 0) {
+            cout << "I hate ";
+        } else {
+            cout << "I love ";
+        }
+        if (i != a - 1) {
+            cout << "that ";
+        } else {
+            cout << "it ";
+        }
+    }
+}
